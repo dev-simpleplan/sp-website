c350bf0c235e429a71e96af2ceee8ed47e4d62c2
dev-simpleplan <simple.website.testing@gmail.com>
2026-07-13 16:31:28 +0530

    footer and header update

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -64,6 +64,21 @@ export default function Footer() {
 
           <Link href="/contact" className={`custom-btn ${styles.spBookCallBtn}`}>
             <span>Book a Call</span>
+            <span className="arrow-wrap">
+                  <svg className="arrow arrow-1" width="12" height="12" viewBox="0 0 12 12" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                      <path
+                            d="M0.878125 11.6667L0 10.7885L9.53854 1.25H3.75V0H11.6667V7.91667H10.4167V2.12813L0.878125 11.6667Z"
+                            fill="currentColor" />
+                  </svg>
+
+                  <svg className="arrow arrow-2" width="12" height="12" viewBox="0 0 12 12" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                      <path
+                            d="M0.878125 11.6667L0 10.7885L9.53854 1.25H3.75V0H11.6667V7.91667H10.4167V2.12813L0.878125 11.6667Z"
+                            fill="currentColor" />
+                  </svg>
+              </span>
           </Link>
         </div>
 
