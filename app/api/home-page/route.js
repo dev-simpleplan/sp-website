0457eaf4a7db81061214051327178be642ab064e
dev-simpleplan <simple.website.testing@gmail.com>
2026-07-22 13:35:23 +0530

    updated api

--- app/api/home-page/route.js
+++ app/api/home-page/route.js
@@ -2,12 +2,15 @@
 
 export async function GET() {
   try {
-    const res = await fetch("http://72.61.235.119:1337/api/home-page?populate=*", {
-      headers: {
-        // Authorization: `Bearer ${process.env.STRAPI_TOKEN}`
-      },
-      cache: "no-store",
-    });
+    const res = await fetch(
+      "http://72.61.235.119:1337/api/home-page?populate[hero][populate]=*&populate[video_section][populate]=*&populate[struggle][populate]=*&populate[our_approach][populate]=*&populate[case_study][populate][case_study_cards][populate]=*&populate[offer_section][populate]=*&populate[stats][populate]=*&populate[service][populate]=*&populate[ready_to_build][populate]=*&populate[testimonials][populate][Testimonials][populate]=*&populate[trusted_section][populate]=*&populate[awards_section][populate][projects][populate]=*&populate[about_section][populate]=*&populate[tools_section][populate][tools][populate]=*&populate[blog_section][populate][blog_posts][populate]=*&populate[pre_footer][populate]=*",
+      {
+        headers: {
+          // Authorization: `Bearer ${process.env.STRAPI_TOKEN}`
+        },
+        cache: "no-store",
+      }
+    );
 
     if (!res.ok) {
       return Response.json(
--- app/components/BringingClarity.js
+++ app/components/BringingClarity.js
@@ -1,23 +1,20 @@
 export default function BringingClarity({ id, data }) {
   if (!data) return null;
 
-  console.log("bullet_item:", data?.bullet_item);
-  console.log("bullet_item:", data);
-
   return (
     <section className="bringing-clarity" id={id}>
       <div className="container">
         <div className="bringing-clarity-in gap-left">
           <div className="heading">
-            <h2 className="reveal-heading">{data.headline}</h2>
+            <h2 className="reveal-heading">{data?.headline}</h2>
           </div>
 
           <div className="list-wrap">
             <ul>
-    {data?.bullet_item?.map((item) => (
-      <li key={item.id}>{item.title}</li>
-    ))}
-  </ul>
+              {data?.bullet_item?.map((item) => (
+                <li key={item?.id}>{item?.title}</li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
